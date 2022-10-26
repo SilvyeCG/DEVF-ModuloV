fc3ee4bfe7ec7bdc10c61445d78d261e71d7eefa
Silvia <scortesg9527@gmail.com>
2022-10-25 21:06:04 -0500

    practice 2

---
+++ 02-netifly/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+import Form from "./components/Form"
+function App() {
+ 
+  return (
+    <div>
+      <div className="container-fluid">
+        <Header />
+      </div>
+      <div className="container-fluid">
+        <Form />
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ 02-netifly/src/components/Form.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <form abineguid="3800496A6F85453F9C0462BBCA78596B">
+  <fieldset>
+    <legend>Legend</legend>
+    <div className="form-group row">
+      <label htmlFor="staticEmail" className="col-sm-2 col-form-label">Email</label>
+      <div className="col-sm-10">
+        <input type="text" readOnly className="form-control-plaintext" id="staticEmail" defaultValue="email@example.com" />
+      </div>
+    </div>
+    <div className="form-group">
+      <label htmlFor="exampleInputEmail1" className="form-label mt-4">Email address</label>
+      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+      <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+      <div id="pwm-inline-icon-26362" className="pwm-field-icon" style={{position: 'absolute !important', width: '18px !important', height: '18px !important', minHeight: '0px !important', minWidth: '0px !important', zIndex: '2147483645 !important', boxShadow: 'none !important', boxSizing: 'content-box !important', background: 'none !important', border: 'none !important', padding: '0px !important', cursor: 'pointer !important', outline: 'none !important', marginTop: '-58px', marginLeft: 524}}><svg style={{display: 'inline-block !important', width: '16px !important', height: '16px !important', fill: 'rgb(230, 0, 23) !important', marginTop: '0.5px !important', position: 'absolute !important', top: '0px !important', left: '0px !important'}} viewBox="0 0 64 64"><g><path d="m20,28.12a33.78,33.78 0 0 1 13.36,2.74a22.18,22.18 0 0 1 0.64,5.32c0,9.43 -5.66,17.81 -14,20.94c-8.34,-3.13 -14,-11.51 -14,-20.94a22.2,22.2 0 0 1 0.64,-5.32a33.78,33.78 0 0 1 13.36,-2.74m0,-28.12c-8.82,0 -14,7.36 -14,16.41l0,5.16c2,-1.2 2,-1.49 5,-2.08l0,-3.08c0,-6.21 2.9,-11.41 8.81,-11.41l0.19,0c6.6,0 9,4.77 9,11.41l0,3.08c3,0.58 3,0.88 5,2.08l0,-5.16c0,-9 -5.18,-16.41 -14,-16.41l0,0zm0,22c-6.39,0 -12.77,0.67 -18.47,4a31.6,31.6 0 0 0 -1.53,9.74c0,13.64 8.52,25 20,28.26c11.48,-3.27 20,-14.63 20,-28.26a31.66,31.66 0 0 0 -1.54,-9.77c-5.69,-3.3 -12.08,-4 -18.47,-4l0,0l0.01,0.03z" /><path d="m21.23,39.5a2.81,2.81 0 0 0 1.77,-2.59a2.94,2.94 0 0 0 -3,-2.93a3,3 0 0 0 -3,3a2.66,2.66 0 0 0 1.77,2.48l-1.77,4.54l6,0l-1.77,-4.5z" /></g></svg><span id="pwm-inline-icon-badge-26362" style={{position: 'absolute !important', inset: 'auto auto 0px 7px !important', boxSizing: 'content-box !important', fontFamily: 'monospace !important', fontSize: '9px !important', borderRadius: '2px !important', color: 'white !important', background: 'rgb(112, 185, 52) !important', border: '0.5px solid white !important', width: 'auto !important', height: '10px !important', minWidth: '10px !important', minHeight: '0px !important', display: 'flex !important', alignItems: 'center !important', justifyContent: 'center !important', pointerEvents: 'none !important'}}>0</span></div></div>
+    <div className="form-group">
+      <label htmlFor="exampleInputPassword1" className="form-label mt-4">Password</label>
+      <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+      <div id="pwm-inline-icon-25434" className="pwm-field-icon" style={{position: 'absolute !important', width: '18px !important', height: '18px !important', minHeight: '0px !important', minWidth: '0px !important', zIndex: '2147483645 !important', boxShadow: 'none !important', boxSizing: 'content-box !important', background: 'none !important', border: 'none !important', padding: '0px !important', cursor: 'pointer !important', outline: 'none !important', marginTop: '-34px', marginLeft: 524}}><svg style={{display: 'inline-block !important', width: '16px !important', height: '16px !important', fill: 'rgb(230, 0, 23) !important', marginTop: '0.5px !important', position: 'absolute !important', top: '0px !important', left: '0px !important'}} viewBox="0 0 64 64"><g><path d="m20,28.12a33.78,33.78 0 0 1 13.36,2.74a22.18,22.18 0 0 1 0.64,5.32c0,9.43 -5.66,17.81 -14,20.94c-8.34,-3.13 -14,-11.51 -14,-20.94a22.2,22.2 0 0 1 0.64,-5.32a33.78,33.78 0 0 1 13.36,-2.74m0,-28.12c-8.82,0 -14,7.36 -14,16.41l0,5.16c2,-1.2 2,-1.49 5,-2.08l0,-3.08c0,-6.21 2.9,-11.41 8.81,-11.41l0.19,0c6.6,0 9,4.77 9,11.41l0,3.08c3,0.58 3,0.88 5,2.08l0,-5.16c0,-9 -5.18,-16.41 -14,-16.41l0,0zm0,22c-6.39,0 -12.77,0.67 -18.47,4a31.6,31.6 0 0 0 -1.53,9.74c0,13.64 8.52,25 20,28.26c11.48,-3.27 20,-14.63 20,-28.26a31.66,31.66 0 0 0 -1.54,-9.77c-5.69,-3.3 -12.08,-4 -18.47,-4l0,0l0.01,0.03z" /><path d="m21.23,39.5a2.81,2.81 0 0 0 1.77,-2.59a2.94,2.94 0 0 0 -3,-2.93a3,3 0 0 0 -3,3a2.66,2.66 0 0 0 1.77,2.48l-1.77,4.54l6,0l-1.77,-4.5z" /></g></svg><span id="pwm-inline-icon-badge-25434" style={{position: 'absolute !important', inset: 'auto auto 0px 7px !important', boxSizing: 'content-box !important', fontFamily: 'monospace !important', fontSize: '9px !important', borderRadius: '2px !important', color: 'white !important', background: 'rgb(112, 185, 52) !important', border: '0.5px solid white !important', width: 'auto !important', height: '10px !important', minWidth: '10px !important', minHeight: '0px !important', display: 'flex !important', alignItems: 'center !important', justifyContent: 'center !important', pointerEvents: 'none !important'}}>0</span></div></div>
+    <div className="form-group">
+      <label htmlFor="exampleSelect1" className="form-label mt-4">Example select</label>
+      <select className="form-select" id="exampleSelect1">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+    </div>
+    <div className="form-group">
+      <label htmlFor="exampleSelect2" className="form-label mt-4">Example multiple select</label>
+      <select multiple className="form-select" id="exampleSelect2">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+    </div>
+    <div className="form-group">
+      <label htmlFor="exampleTextarea" className="form-label mt-4">Example textarea</label>
+      <textarea className="form-control" id="exampleTextarea" rows={3} defaultValue={""} />
+    </div>
+    <div className="form-group">
+      <label htmlFor="formFile" className="form-label mt-4">Default file input example</label>
+      <input className="form-control" type="file" id="formFile" />
+    </div>
+    <fieldset className="form-group">
+      <legend className="mt-4">Radio buttons</legend>
+      <div className="form-check">
+        <input className="form-check-input" type="radio" name="optionsRadios" id="optionsRadios1" defaultValue="option1" defaultChecked />
+        <label className="form-check-label" htmlFor="optionsRadios1">
+          Option one is this and that—be sure to include why it's great
+        </label>
+      </div>
+      <div className="form-check">
+        <input className="form-check-input" type="radio" name="optionsRadios" id="optionsRadios2" defaultValue="option2" />
+        <label className="form-check-label" htmlFor="optionsRadios2">
+          Option two can be something else and selecting it will deselect option one
+        </label>
+      </div>
+      <div className="form-check disabled">
+        <input className="form-check-input" type="radio" name="optionsRadios" id="optionsRadios3" defaultValue="option3" disabled />
+        <label className="form-check-label" htmlFor="optionsRadios3">
+          Option three is disabled
+        </label>
+      </div>
+    </fieldset>
+    <fieldset className="form-group">
+      <legend className="mt-4">Checkboxes</legend>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefault" />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+          Default checkbox
+        </label>
+      </div>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" defaultValue id="flexCheckChecked" defaultChecked />
+        <label className="form-check-label" htmlFor="flexCheckChecked">
+          Checked checkbox
+        </label>
+      </div>
+    </fieldset>
+    <fieldset className="form-group">
+      <legend className="mt-4">Switches</legend>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Default switch checkbox input</label>
+      </div>
+      <div className="form-check form-switch">
+        <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" defaultChecked />
+        <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Checked switch checkbox input</label>
+      </div>
+    </fieldset>
+    <fieldset className="form-group">
+      <legend className="mt-4">Ranges</legend>
+      <label htmlFor="customRange1" className="form-label">Example range</label>
+      <input type="range" className="form-range" id="customRange1" />
+      <label htmlFor="disabledRange" className="form-label">Disabled range</label>
+      <input type="range" className="form-range" id="disabledRange" disabled />
+      <label htmlFor="customRange3" className="form-label">Example range</label>
+      <input type="range" className="form-range" min={0} max={5} step="0.5" id="customRange3" />
+    </fieldset>
+    <button type="submit" className="btn btn-primary">Submit</button>
+  </fieldset>
+</form>
+
+  )
+}
+
+export default Form
